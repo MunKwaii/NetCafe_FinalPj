f05d4accac003e4b801ee877ad6d86e7931ba1d3
Khánh <Quockhan6310@gmail.com>
2025-03-15 13:25:30 +0700

    Second commit NetCafeManager

---
+++ Data/UserManagement.sql
@@ -0,0 +1,15 @@
+﻿USE UserManagement;  
+
+CREATE TABLE Users (
+    ID INT IDENTITY(1,1) PRIMARY KEY,  
+    Username NVARCHAR(50) UNIQUE NOT NULL, 
+    Password NVARCHAR(255) NOT NULL,  
+    Role NVARCHAR(20) NOT NULL CHECK (Role IN ('Manager', 'Employee', 'Customer')) 
+);
+
+INSERT INTO Users (Username, Password, Role)
+VALUES 
+    ('manager', 'manager', 'Manager'),  
+    ('employee', 'employee', 'Employee'),  
+    ('customer', 'customer', 'Customer');
+
